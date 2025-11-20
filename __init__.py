7d77a1302c90b5e902603103b5a95bf7fa009bf5
scferro <stephencferro@gmail.com>
2025-11-20 14:42:41 -0600

    removed cli, cleaned up readme

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ PX4 Agent - Intelligent drone mission planning with LangChain and Granite 3.3 2B
 """
 
 from .core import PX4Agent
-from .cli import OutputFormatter
 from .config import get_settings, reload_settings
 
 __version__ = "0.1.0"
@@ -11,7 +10,6 @@ __author__ = "PX4 Agent Team"
 
 __all__ = [
     "PX4Agent",
-    "OutputFormatter", 
     "get_settings",
     "reload_settings"
 ]
--- cli/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-CLI Module for PX4 Agent
-"""
-
-from .output import OutputFormatter
-
-__all__ = [
-    'OutputFormatter'
-]
